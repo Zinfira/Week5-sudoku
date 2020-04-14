9960c208494be57963d300a3712588edbdd5191d
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-14 15:00:35 -0700

    Number push to row array and test is failed because we need to create a function

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -5,9 +5,15 @@ describe('Sudoku', () => {
     var sudoku = new Sudoku()
     expect(sudoku.row).toEqual([])
   });
-
+//Test 2
   test('should check if array for column is empty', () => {
     var sudoku = new Sudoku()
     expect(sudoku.column).toEqual([])
+  });
+
+  test('number push into empty array', () => {
+    var sudoku = new Sudoku()
+    sudoku.addToRows(1)
+    expect(sudoku.row).toEqual([1])
   });
 });
--- src/sudoku.js
+++ src/sudoku.js
@@ -14,3 +14,4 @@ export function Sudoku() {
   this.row = [];
   this.column = [];
 }
+
